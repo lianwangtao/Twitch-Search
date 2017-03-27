13a38b774cfd05870e3d27ee43029d84777e7c5d
lianwangtao <wlian3@wisc.edu>
2017-03-27 13:43:28 -0500

    Server foundation

--- server.babel.js
+++ server.babel.js
@@ -1,8 +1,13 @@
 import express from 'express'
 
 const app = express()
+const cliend_id = 'vs5moi1agb6bb7opywk2chpimxxr76'
 
 app.use('/', express.static('public'))
+
+app.get('/search', (req, res) => {
+
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${process.env.PORT || 3000}`)
