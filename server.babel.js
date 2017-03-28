a8af874f681139404852d73c3c31c5fb71f6e8b7
lianwangtao <wlian3@wisc.edu>
2017-03-28 00:13:33 -0500

    Successfully get results

--- server.babel.js
+++ server.babel.js
@@ -3,6 +3,9 @@ import request from 'request'
 import bodyParser from 'body-parser'
 
 const app = express()
+const PORT = process.env.PORT || 3000
+const cliend_id = 'vs5moi1agb6bb7opywk2chpimxxr76'
+
 // Support encoded bodies
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,30 +18,34 @@ app.use((req, res, next) => {
 app.use('/', express.static('public'))
 
 app.get('/search', (req, res) => {
-  // const keyword = req.body.keyword
-  // console.log('req:', req.body)
-  // console.log('keyword:', keyword)
-  const cliend_id = 'vs5moi1agb6bb7opywk2chpimxxr76'
-  const url = 'https://api.twitch.tv/kraken/search/streams?q='
-  const keyword = 'startcraft'
+  const keyword = req.query.keyword
+  // const offset = req.query.offset
+  console.log('req:', req.query)
+  console.log('keyword:', keyword)
+  const url = 'https://api.twitch.tv/kraken/search/'
+  const query = 'streams?q=' + keyword
+  const limit  = '&limit=10'
 
   request({
-    url: url + keyword,
+    url: url + query + limit,
     headers: {
       'Client-ID': cliend_id,
       'Content-type': 'application/json',
     },
   }, function (error, response, body) {
     if (error) {
+      console.log('Error:',error)
       res.send(400, 'Error in getting search result!')
+      return
     }
     if (response.statusCode === 200) {
       const results = body
-      res.send(200, results)
+      res.status(200).send(results)
+      return
     }
   })
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${process.env.PORT || 3000}`)
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
 })
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {
   AppBar,
   TextField,
+  RaisedButton,
 } from 'material-ui'
 import {
   grey100,
@@ -14,6 +15,7 @@ import {
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import axios from 'axios'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -32,6 +34,28 @@ const muiTheme = getMuiTheme({
 export default class App extends React.Component {
   constructor(props) {
     super(props)
+    this.handleKeyword = this.handleKeyword.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+
+    this.state = {
+      keyword: ''
+    }
+  }
+
+  handleKeyword(event) {
+    this.setState({ keyword: event.target.value })
+  }
+
+  handleSearch(event) {
+    console.log('Keyword:', this.state.keyword)
+    axios.get('/search', {
+      params: {
+        keyword: this.state.keyword,
+      },
+    }).then((response) => {
+      console.log('response:', response)
+      this.setState({ resultsData: response.data })
+    })
   }
 
   buildSearchBar() {
@@ -40,39 +64,104 @@ export default class App extends React.Component {
         style={{
           backgroundColor: 'white',
           padding: 30,
-          textAlign: 'center',
+          display: 'flex',
         }}
         >
-        <i
-          className="material-icons"
-          style={{
-            color: 'grey',
-            position: 'relative',
-            bottom: -8,
-          }}
-          >
-          search
-        </i>
-        <TextField
-          autoFocus
-          hintText="Search for a stream channel here"
-        />
+          <div
+            style={{
+            }}
+            >
+            <TextField
+              autoFocus
+              value={this.state.keyword}
+              onChange={this.handleKeyword}
+              hintText="Search for a stream channel here"
+            />
+          </div>
+          <div
+            style={{
+              paddingLeft: 10,
+              paddingTop: 5,
+            }}
+            >
+            <RaisedButton
+              href=""
+              target="_blank"
+              label="Search"
+              primary
+              onClick={this.handleSearch}
+              icon={
+                <i
+                  className="material-icons"
+                  style={{
+                    color: 'white',
+                  }}
+                  >
+                  search
+                </i>
+              }
+              />
+          </div>
       </div>
     )
   }
 
+  buildResultList() {
+    const results = []
+    if (this.state.resultsData && this.state.resultsData.streams) {
+      for (const stream of this.state.resultsData.streams) {
+        console.log('Stream:', stream)
+        const result = (
+          <div
+            style={{
+              display: 'flex',
+            }}
+            >
+            <img
+              style={{
+                flex: '1 1 0',
+                width: '60%',
+                height: 'auto',
+              }}
+              src={stream.preview.medium}
+              />
+            <div
+              style={{
+                flex: '3 1 0',
+              }}
+              >
+              <h2>{stream.channel.display_name}</h2>
+              <h3>{stream.game} - {stream.viewers} viewers</h3>
+            </div>
+          </div>
+        )
+        results.push(result)
+      }
+    }
+    return results
+  }
+
   buildResults() {
-    return(
-      <ul>
-        <li> Result 1 </li>
-        <li> Result 2 </li>
-      </ul>
-    )
+    if (this.state.resultsData) {
+      const resultList = this.buildResultList()
+      const resultCount = this.state.resultsData._total
+      console.log('')
+      return(
+        <div>
+          <div>
+            <h3>Total results: {resultCount}</h3>
+          </div>
+          {resultList}
+        </div>
+      )
+    }
+    return "Type something to start searching!"
   }
 
   render() {
     const searchBar = this.buildSearchBar()
     const results = this.buildResults()
+
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div>
